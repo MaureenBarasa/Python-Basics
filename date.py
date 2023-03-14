b3605e4c62d3e1cc116b5fe0f593aa98f04b32ef
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2023-03-14 12:28:55 +0300

    updated date.py

--- date.py
+++ date.py
@@ -1,9 +1,18 @@
-#function to print current date and time
 
 import datetime
+
+import pytz
+
+#function to print current time and date
 
 def currentdate ():
     now = datetime.datetime.now()
     print("The current date and time is", now)
 currentdate()
 
+#function to output all the available timezones of the world
+def timezones ():
+    zones = pytz.all_timezones
+    print(zones)
+
+timezones() 

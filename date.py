f6902755350775233161e2bb482c1edb5cfb494c
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2023-03-14 12:20:32 +0300

    created date.py

---
+++ date.py
@@ -0,0 +1,9 @@
+#function to print current date and time
+
+import datetime
+
+def currentdate ():
+    now = datetime.datetime.now()
+    print("The current date and time is", now)
+currentdate()
+
